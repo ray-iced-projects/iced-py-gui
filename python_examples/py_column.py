806bde1770cc6394e727ea1e3b594165806264eb
icedpygui <icedpygui@gmail.com>
2026-03-07 17:38:28 -0500

    column example updated

--- python_examples/py_column.py
+++ python_examples/py_column.py
@@ -3,6 +3,8 @@ from imports import *
 # Column and add_column() Demo
 # A column adds widgets vertically
 
+# NOTE: Toggle the debug at the top of the window
+# to show the outline of the columns and widgets.
 
 # A function to toggle the the debug
 # or outline mode
@@ -12,8 +14,6 @@ def toggle_debug(tog_id: int, value: bool, wnd_id: int):
 
 
 # Add the window
-# Uncomment the debug parameter to see
-# the container and widget outlines
 with Window(
     title="Column", 
     size=(600, 600),  
@@ -28,7 +28,7 @@ with Window(
         
         add_text(content="Spacing of 10.0 and 20.0")
         
-        with Row(width_fill=True, height=100.0, spacing=20.0):
+        with Row(width_fill=True, height=75.0, spacing=20.0):
             # We use add_column here to show the alternate version
             # We add two text widgets to each column below to demonstrate spacing
             add_column(id="col1", spacing=10.0)
@@ -39,8 +39,9 @@ with Window(
             add_text(parent_id="col2", content="Text in Column 2")
             add_text(parent_id="col2", content="Text in Column 2")
 
-        add_text(content="Padding of [all] and [top, right, botton, left]")
-        add_text(content="Toggle the debug mode and Note the space\n around the Column outline", align_x=AlignX.Left)
+        add_text(content="Padding of [all] and [top, right, botton, left]\n" +
+                "Note the space around the Column outline", 
+                align_x=AlignX.Left)
         
         with Row(width_fill=True, height=100.0, spacing=20.0):
             add_column(id="col3", padding=[20.0])
@@ -52,19 +53,20 @@ with Window(
             add_text(parent_id="col4", content="Text in Column 4")
         
         add_text(content="Alignment parameter = align\n" +
-                 "values = Align.Start(default), Align.Center, Align.End",
+                 "values = Align.Start(default), Align.Center, Align.End\n" +
+                 "NOTE the empty space(s) beside each text group",
                  align_x=AlignX.Left)
         
-        with Row(width_fill=True, height=100.0, spacing=20.0):
-            add_column(id="col5", align=Align.Start, height_fill=True)
+        with Row(width_fill=True, height=75.0, spacing=20.0):
+            add_column(id="col5", align_x=AlignX.Left, width=150.0)
             add_text(parent_id="col5", content="Text in Column 5")
             add_text(parent_id="col5", content="Text in Column 5")
             
-            add_column(id="col6", align=Align.Center, height_fill=True)
+            add_column(id="col6", align_x=AlignX.Center, width=150.0)
             add_text(parent_id="col6", content="Text in Column 6")
             add_text(parent_id="col6", content="Text in Column 6")
             
-            add_column(id="col7", align=Align.End, height_fill=True)
+            add_column(id="col7", align_x=AlignX.Right, width=150.0)
             add_text(parent_id="col7", content="Text in Column 7")
             add_text(parent_id="col7", content="Text in Column 7")
 
--- src/widgets/ipg_column.rs
+++ src/widgets/ipg_column.rs
@@ -1,11 +1,9 @@
 //! ipg_column
 use crate::app::Message;
 use crate::py_api::helpers::get_padding;
-use crate::widgets::enums::Align;
+use crate::widgets::enums::AlignX;
 use crate::widgets::widget_param_update::{
-    WidgetParamUpdate,
-    set_opt_bool, set_opt_f32, set_opt_vec_f32,
-    set_width, set_width_fill, set_height, set_height_fill, set_align,
+    WidgetParamUpdate, set_halign, set_height, set_height_fill, set_opt_bool, set_opt_f32, set_opt_vec_f32, set_width, set_width_fill
 };
 
 use iced::{Element, Length};
@@ -24,7 +22,7 @@ pub struct IpgColumn {
     pub width: Length,
     pub height: Length,
     pub max_width: Option<f32>,
-    pub align: Option<Align>,
+    pub align_x: Option<AlignX>,
     pub clip: Option<bool>,
 }
 
@@ -40,7 +38,7 @@ impl IpgColumn {
                 .height(self.height);
         
         let col = 
-            if let Some(align_x) = &self.align {
+            if let Some(align_x) = &self.align_x {
                     col.align_x(align_x.to_iced())
                 } else { col };
 
@@ -84,7 +82,7 @@ impl WidgetParamUpdate for IpgColumn {
 
     fn param_update(&mut self, param: Self::Param, value: &PyObject, name: String) {
         match param {
-            IpgColumnParam::AlignX     => set_align(&mut self.align, value, name),
+            IpgColumnParam::AlignX     => set_halign(&mut self.align_x, value, name),
             IpgColumnParam::Clip       => set_opt_bool(&mut self.clip, value, name),
             IpgColumnParam::Padding    => set_opt_vec_f32(&mut self.padding, value, name),
             IpgColumnParam::Width      => set_width(&mut self.width, value, name),

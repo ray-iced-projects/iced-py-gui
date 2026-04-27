6bbf620d5a7571e8da1d4214f15cedc70f993232
ray-5050 <ray.iced.projects@gmail.com>
2026-04-27 19:56:25 -0400

    added py util

--- py utils/make_enums/main.py
+++ py utils/make_enums/main.py
@@ -1,6 +1,4 @@
-
-
-snake = [
+snake_case = [
     "background_color: Option<Color>",
     "background_color_alpha: Option<f32>",
     "background_rgba: Option<[f32; 4]>",
@@ -59,16 +57,14 @@ snake = [
     "snap: Option<bool>",
 ]
 
+param = "ButtonStyleParam::"
+
 def to_pascal(snake: str) -> str:
     return "".join(word.capitalize() for word in snake.split("_"))
 
-# enum_variants = sorted(
-#     to_pascal(entry.split(":")[0].strip())
-#     for entry in snake_case
-# )
 
-# for variant in enum_variants:
-#     print(variant + ",")
 
-for entry in snake:
-    print(entry)
+for entry in snake_case:
+    split_entry = entry.split(":")[0].strip()
+    wd = to_pascal(split_entry)
+    print(f"{param}{wd} => set_t_value(&mut self.{split_entry}, value, \"{param}{wd}\"),")

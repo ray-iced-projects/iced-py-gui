dcdb17ef81fa3f25d0bfeb1105c68219bb0b9870
ray-5050 <ray.iced.projects@gmail.com>
2026-06-09 08:48:49 -0400

    styling wip

--- python_examples/py_button_styling.py
+++ python_examples/py_button_styling.py
@@ -19,6 +19,7 @@ from icedpygui import (
     add_text,
     TextParam,
     update_widget,
+    update_widget_params,
     get_styling_palette,
     StdColorStyle,
     window_theme_names,
@@ -33,9 +34,11 @@ def on_theme_select(_pl_id: int, theme_name: str):
     """Select a Theme by PickList"""
     update_widget(wnd_id, WindowParam.Theme, theme_name)
     state["selected_theme"] = theme_name
+    state["palette"] = get_styling_palette(state["selected_theme"], StdColorStyle.Primary)
     std_colors_selected(0, 0)
-    update_background(theme_name)
-    update_widget(rd_id, RadioParam.SelectedIndex, 0)
+    update_widget(rd_id_colors, RadioParam.SelectedIndex, 0)
+    std_styles_selected(0,0)
+    update_widget(rd_id_styles, RadioParam.SelectedIndex, 0)
 
 
 def color_selected(_cp_id: int, color: list[float]):
@@ -51,78 +54,80 @@ def std_colors_selected(_rd_id: int, index: int):
     match index:
         case 0:
             state["selected_style"] = std_colors[0]
-            palettes = get_styling_palette(state["selected_theme"], StdColorStyle.Primary)
+            state["palette"] = get_styling_palette(state["selected_theme"], StdColorStyle.Primary)
         case 1:
             state["selected_style"] = std_colors[1]
-            palettes = get_styling_palette(state["selected_theme"], StdColorStyle.Secondary)
+            state["palette"] = get_styling_palette(state["selected_theme"], StdColorStyle.Secondary)
         case 2:
             state["selected_style"] = std_colors[2]
-            palettes = get_styling_palette(state["selected_theme"], StdColorStyle.Success)
+            state["palette"] = get_styling_palette(state["selected_theme"], StdColorStyle.Success)
         case 3:
             state["selected_style"] = std_colors[3]
-            palettes = get_styling_palette(state["selected_theme"], StdColorStyle.Warning)
+            state["palette"] = get_styling_palette(state["selected_theme"], StdColorStyle.Warning)
         case 4:
             state["selected_style"] = std_colors[4]
-            palettes = get_styling_palette(state["selected_theme"], StdColorStyle.Danger)
+            state["palette"] = get_styling_palette(state["selected_theme"], StdColorStyle.Danger)
 
-    for (idx, key) in enumerate(palette_styling_keys):
+    for (key, label) in palette_labels.items():
         if key == "base_alpha_0.5":
             # special case for button disable status
-            (bkg_rgba, text_rgba) = palettes["base"]
+            (bkg_rgba, text_rgba) = state["palette"]["base"]
+            bkg_rgba = bkg_rgba.copy()
+            text_rgba = text_rgba.copy()
             bkg_rgba[3] *= 0.5
             text_rgba[3] *= 0.5
         else:
-            (bkg_rgba, text_rgba) = palettes[key]
+            (bkg_rgba, text_rgba) = state["palette"][key]
 
-        if border_styling[idx]:
-            style_id = get_container_border_style(bkg_rgba, palettes["bkg_stronger"][1])
-            update_widget(cont_ids[key], ContainerParam.StyleId, style_id)
-            update_widget(text_ids[key], TextParam.ColorRgba, text_rgba)
+        if key != label:
+            style_id = get_container_border_style(bkg_rgba, state["palette"]["bkg_stronger"][1])
         else:
             style_id = add_container_style(bkg_rgba=bkg_rgba)
-            update_widget(cont_ids[key], ContainerParam.StyleId, style_id)
-            update_widget(text_ids[key], TextParam.ColorRgba, text_rgba)
+
+        update_widget(cont_ids[key], ContainerParam.StyleId, style_id)
+        update_widget(text_ids[key], TextParam.ColorRgba, text_rgba)
 
 
 def std_styles_selected(_rd_id: int, index: int):
     """Standard styling from radio selection"""
-    labels = []
     match index:
         case 0:
             labels = subtle_labels
-            border = subtle_border_bkg_styling
         case 1:
-            pass
+            labels = bkg_labels
 
-    for (idx, key) in enumerate(palette_bkg_keys):
-        # Update the text values
-        update_widget(bkg_text_ids[key], TextParam.Content, labels[key])
-        if border[idx]:
-            style_id = add_container_style()
-            update_widget(cont_ids[key], ContainerParam.StyleId, style_id)
-
-
-def update_background(theme: str):
-    """Update the background containers"""
-    palettes = get_styling_palette(theme, StdColorStyle.Primary)
-    for key in palette_bkg_keys:
+    # add border if palette used by style
+    for (key, label )in labels.items():
+        # the palette keys contain the bkg_rgba and the text_color
         if key == "bkg_weakest_alpha_0.5":
-            (bkg_rgba, text_rgba) = palettes["bkg_weakest"]
+            (bkg_rgba, text_rgba) = state["palette"]["bkg_weakest"]
+            bkg_rgba = bkg_rgba.copy()
+            text_rgba = text_rgba.copy()
             bkg_rgba[3] *= 0.5
             text_rgba[3] *=0.5
         else:
-            (bkg_rgba, text_rgba) = palettes[key]
+            (bkg_rgba, text_rgba) = state["palette"][key]
 
-        style_id = add_container_style(bkg_rgba=bkg_rgba)
+        if key != label:
+            style_id = get_container_border_style(
+                            bkg_rgba,
+                            state["palette"]["bkg_strongest"][1]) # border color
+        else:
+            style_id = add_container_style(bkg_rgba=bkg_rgba) # no border
+
         update_widget(bkg_cont_ids[key], ContainerParam.StyleId, style_id)
-        update_widget(bkg_text_ids[key], TextParam.ColorRgba, text_rgba)
+        update_widget_params(
+            bkg_text_ids[key], {
+                TextParam.ColorRgba: text_rgba,
+                TextParam.Content: label,
+                })
 
 
-def get_container_border_style(bkg_rgba: list, stronger: list) -> int:
+def get_container_border_style(bkg_rgba: list, color_rgba: list) -> int:
     """get the container style with a border"""
     return add_container_style(
             bkg_rgba=bkg_rgba,
-            border_rgba=stronger,
+            border_rgba=color_rgba,
             border_width=2.0)
 
 
@@ -136,15 +141,13 @@ text_ids = {}   # {variant_name: text_id}
 bkg_cont_ids = {}   # {variant_name: cont_id}
 bkg_text_ids = {}   # {variant_name: text_id}
 state = {"selected_style": "",
-         "selected_theme": "TokyoNight"}
+         "selected_theme": "TokyoNight",
+         "palette": get_styling_palette("TokyoNight", StdColorStyle.Primary)}
 
 # The bkg keys are used for styling in some cases
 palette_bkg_keys = ("bkg_base", "bkg_weak", "bkg_weaker", "bkg_weakest", "bkg_neutral",
                 "bkg_strong", "bkg_stronger", "bkg_strongest", "bkg_weakest_alpha_0.5")
 
-# Button specific styling for Subtle
-subtle_border_bkg_styling = (False, False, True, True, False,
-                      True, False, False, True)
 subtle_labels = {
     "bkg_base": "bkg_base",
     "bkg_weak": "bkg_weak",
@@ -156,10 +159,25 @@ subtle_labels = {
     "bkg_strongest": "bkg_strongest",
     "bkg_weakest_alpha_0.5": "bkg_weakest\nalpha_0.5\nDisabled"}
 
+bkg_labels = {
+    "bkg_base": "bkg_base\nActive",
+    "bkg_weak": "bkg_weak\nHovered",
+    "bkg_weaker": "bkg_weaker",
+    "bkg_weakest": "bkg_weakest",
+    "bkg_neutral": "bkg_neutral",
+    "bkg_strong": "bkg_strong\nPressed",
+    "bkg_stronger": "bkg_stronger",
+    "bkg_strongest": "bkg_strongest",
+    "bkg_weakest_alpha_0.5": "bkg_weakest\nalpha_0.5\nDisabled"}
+
 # Button specific styling
-palette_styling_keys = ("base", "weak", "strong", "base_alpha_0.5")
+palette_labels = {
+    "base": "base\nActive/Pressed",
+    "weak": "weak",
+    "strong": "strong\nHovered",
+    "base_alpha_0.5": "base\nalpha_0.5\nDisabled"
+    }
 border_styling = (True, False, True, True)
-palette_labels = ("base\nActive/Pressed", "weak", "strong\nHovered", "base\nalpha_0.5\nDisabled")
 
 
 # ---------------------------------------------------------------------------
@@ -179,7 +197,7 @@ with Window(title="Button Styling", size=(700, 800), center=True) as wnd_id:
             "if theme=GRUVBOX_LIGHT, primary=light blue, \n" +
             "if theme=GRUVBOX_DARK, primary=dark blue")
 
-        rd_id = add_radio(
+        rd_id_colors = add_radio(
             labels=std_colors,
             selected_index=0,
             horizontal=True,
@@ -188,21 +206,10 @@ with Window(title="Button Styling", size=(700, 800), center=True) as wnd_id:
             on_selected=std_colors_selected,
         )
 
-        rd_id = add_radio(
-            labels=std_styles,
-            selected_index=None,
-            horizontal=True,
-            radio_spacing=10,
-            radio_wrap=True,
-            on_selected=std_styles_selected,
-        )
-
         add_text(content="Button palettes used for the button statuses, border highlighted ones")
 
         with Row(spacing=20, wrap=True):
-            for (index, key) in enumerate(palette_styling_keys):
-                label = palette_labels[index]
-
+            for (key, label) in palette_labels.items():
                 if key == "base_alpha_0.5":
                     # special case for button disable status
                     (bkg_rgba, text_rgba) = palettes["base"]
@@ -211,38 +218,51 @@ with Window(title="Button Styling", size=(700, 800), center=True) as wnd_id:
                 else:
                     (bkg_rgba, text_rgba) = palettes[key]
 
-                if border_styling[index]:
-                    style_id = get_container_border_style(bkg_rgba, palettes["bkg_stronger"][1])
+                if key != label:
+                    style_id = get_container_border_style(
+                                    bkg_rgba,
+                                    palettes["bkg_stronger"][1]) # border color
                 else:
-                    style_id = add_container_style(bkg_rgba=bkg_rgba)
+                    style_id = add_container_style(bkg_rgba=bkg_rgba) # no border
 
                 with Container(width=120, height=60,
                                 align_center=True, style_id=style_id) as _cont_id:
                     cont_ids[key] = _cont_id
                     text_ids[key] = add_text(content=label, color_rgba=text_rgba, size=14)
 
-
-
         add_text(content="Background palettes which may or may not be used for statuses\n" +
                  "They may be used for additional styling such as Subtle or Background, etc.")
 
-        with Row(spacing=20, wrap=True):
-            content = ""
-            for (idx, key )in enumerate(palette_bkg_keys):
-                # the palette keys contain the bkg_rgba and the text_color
+        rd_id_styles = add_radio(
+            labels=std_styles,
+            selected_index=0,
+            horizontal=True,
+            radio_spacing=10,
+            radio_wrap=True,
+            on_selected=std_styles_selected,
+        )
 
+        with Row(spacing=20, wrap=True):
+            for (key, label )in subtle_labels.items():
+                # the palette keys contain the bkg_rgba and the text_color
                 if key == "bkg_weakest_alpha_0.5":
                     (bkg_rgba, text_rgba) = palettes["bkg_weakest"]
                     bkg_rgba[3] *= 0.5
                     text_rgba[3] *=0.5
-                    content = "bkg_weakest\nalpha_0.5"
+                    style_id = add_container_style(bkg_rgba=bkg_rgba)
                 else:
                     (bkg_rgba, text_rgba) = palettes[key]
-                    content = key
-                style_id = add_container_style(bkg_rgba=bkg_rgba)
+
+                if key != label:
+                    style_id = get_container_border_style(
+                                    bkg_rgba,
+                                    palettes["bkg_strongest"][1]) # border color
+                else:
+                    style_id = add_container_style(bkg_rgba=bkg_rgba) # no border
+
                 with Container(width=120, height=60,
                                 align_center=True, style_id=style_id) as _cont_id:
                     bkg_cont_ids[key] = _cont_id
-                    bkg_text_ids[key] = add_text(content=content, color_rgba=text_rgba, size=14)
+                    bkg_text_ids[key] = add_text(content=label, color_rgba=text_rgba, size=14)
 
 start_session()
